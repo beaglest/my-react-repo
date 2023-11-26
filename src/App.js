9a84fad623c8c9d9905d62eef18d292f01b3905b
beaglest <39458964+beaglest@users.noreply.github.com>
2023-11-26 18:35:17 +0300

    Add Props for multiple names

--- src/App.js
+++ src/App.js
@@ -3,8 +3,10 @@ import Person from './Person'
 
 function App() {
   return (
-    <div className="App">
-      <Person></Person>
+    <div>
+      <Person name='Metin'/>
+      <Person name='Deniz'/>
+      <Person name='Aras'/>
     </div>
   );
 }
--- src/Person.js
+++ src/Person.js
@@ -1,27 +1,31 @@
 import React, { Component } from "react";
 
 class Person extends Component {
+    constructor (props) {
+        super(props);
+        this.props = props;
+    }
     render() {
 
-        // return(
-        // <div className='AppHeader'>
-        //     <i><h1>Hello GfG</h1></i>
-        //     </div>
-        // )
-        return React.createElement(
-            "div",
-            {class: "App-header"},
-            React.createElement(
-                "i",
-                null,
-                React.createElement(
-                    "h1",
-                    null,
-                    "Hello GfG"
+        return(
+        <div>
+            <i><h1>Hello {this.props.name}, This is FS Class.</h1></i>
+            </div>
+        )
+        // return React.createElement(
+        //     "div",
+        //     {class: ""},
+        //     React.createElement(
+        //         "i",
+        //         null,
+        //         React.createElement(
+        //             "h1",
+        //             null,
+        //             "Hello GfG"
 
-                )
-            )
-        );
+        //         )
+        //     )
+        // );
         
     }
 }
